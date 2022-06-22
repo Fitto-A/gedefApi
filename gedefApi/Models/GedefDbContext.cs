a05066149d039d1ae279ec50bddc3e091f6137fb
agustin <afittipaldi@grupoveraz.com.ar>
2022-06-22 14:28:30 -0300

    2do commit

---
+++ gedefApi/Models/GedefDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace gedefApi.Models
+{
+    public class GedefDbContext : DbContext
+    {
+        public GedefDbContext(DbContextOptions<GedefDbContext> options) : base(options)
+        { }
+
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Legajo> Legajos { get; set; }
+    }
+
+}
