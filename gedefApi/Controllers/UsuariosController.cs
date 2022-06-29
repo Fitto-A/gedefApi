e77768918742a22ecf4511a87433b386fe4b557e
agustin <afittipaldi@grupoveraz.com.ar>
2022-06-29 13:17:04 -0300

    Usuarios controller

--- gedefApi/Controllers/UsuariosController.cs
+++ gedefApi/Controllers/UsuariosController.cs
@@ -24,21 +24,21 @@ namespace gedefApi.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Usuarios>>> GetTBA_USUARIOS()
         {
-          if (_context.TBA_USUARIOS == null)
-          {
-              return NotFound();
-          }
+            if (_context.TBA_USUARIOS == null)
+            {
+                return NotFound();
+            }
             return await _context.TBA_USUARIOS.ToListAsync();
         }
 
-        // GET: api/Usuarios/5
+        //GET: api/Usuarios/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Usuarios>> GetUsuarios(int id)
         {
-          if (_context.TBA_USUARIOS == null)
-          {
-              return NotFound();
-          }
+            if (_context.TBA_USUARIOS == null)
+            {
+                return NotFound();
+            }
             var usuarios = await _context.TBA_USUARIOS.FindAsync(id);
 
             if (usuarios == null)
@@ -48,6 +48,24 @@ namespace gedefApi.Controllers
 
             return usuarios;
         }
+
+        //[HttpGet("{USUARIO}")]
+        //public async Task<ActionResult<Usuarios>> GetUsuarios(int USUARIO)
+
+        //{
+        //    if (_context.TBA_USUARIOS == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    var usuarios = await _context.TBA_USUARIOS.FindAsync(USUARIO);
+
+        //    if (usuarios == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return usuarios;
+        //}
 
         // PUT: api/Usuarios/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
@@ -85,10 +103,10 @@ namespace gedefApi.Controllers
         [HttpPost]
         public async Task<ActionResult<Usuarios>> PostUsuarios(Usuarios usuarios)
         {
-          if (_context.TBA_USUARIOS == null)
-          {
-              return Problem("Entity set 'GedefDbContext.TBA_USUARIOS'  is null.");
-          }
+            if (_context.TBA_USUARIOS == null)
+            {
+                return Problem("Entity set 'GedefDbContext.TBA_USUARIOS'  is null.");
+            }
             _context.TBA_USUARIOS.Add(usuarios);
             await _context.SaveChangesAsync();
 
