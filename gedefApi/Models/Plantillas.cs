3fb1def7128e51fe1029eb426b7838a862a00fad
agustin <afittipaldi@grupoveraz.com.ar>
2022-07-01 09:13:02 -0300

    .

--- gedefApi/Models/Plantillas.cs
+++ gedefApi/Models/Plantillas.cs
@@ -7,14 +7,13 @@ namespace gedefApi.Models
     {
         [Key]
         public int CODPLA { get; set; }
-        public int CODROLXBAR { get; set; }
-
         [Column(TypeName = "nvarchar(50)")]
         public string NOMBAR { get; set; }
+        public int CODROLXBAR { get; set; }
+        public int NUMTRIP { get; set; }
 
         [Column(TypeName = "nvarchar(50)")]
         public string ARMADOR { get; set; }
-
         public int CODMAR { get; set; }
     }
 }
