b71e4e42401d01e9cd87cf55f851e8e537059b69
Fitto-A <agustinfittipaldi.dg@gmail.com>
2023-04-19 13:00:11 -0300

    added new roles

--- gedefApi/Models/PlanillaRoles/RolxBar.cs
+++ gedefApi/Models/PlanillaRoles/RolxBar.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace gedefApi.Models.PlanillaRoles
 {
+
     public class RolxBar
     {
         [Key]
@@ -11,10 +12,16 @@ namespace gedefApi.Models.PlanillaRoles
         public int CODBAR { get; set; }
 
         [Column(TypeName = "nchar(1)")]
+        public string? CAPITAN { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
         public string? PATRON { get; set; }
-        
+
         [Column(TypeName = "nchar(1)")]
         public string? SEGUNPATRON { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? PRIMOFCUBIERTA { get; set; }
 
         [Column(TypeName = "nchar(1)")]
         public string? JEFEMAQUINAS { get; set; }
@@ -23,7 +30,64 @@ namespace gedefApi.Models.PlanillaRoles
         public string? SEGUNJEFEMAQUINAS { get; set; }
 
         [Column(TypeName = "nchar(1)")]
+        public string? PRIMOFMAQUINAS { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? SEGUNOFMAQUINAS { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? PRIMOFCONDUCTOR { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? SEGUNOFCONDUCTOR { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? PRIMPESCADOR { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? SEGUNPESCADOR { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? CONTRAMCUBIERTA { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? CONTRAMPLANTA { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
         public string? COCINERO { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? COCINERO2 { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? ENGRASADOR1 { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? ENGRASADOR2 { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? BODEGUERO1 { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? BODEGUERO2 { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? TUNELERO1 { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? TUNELERO2 { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? GUINCHERO1 { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? GUINCHERO2 { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? LAVADOR { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? ENFERMERO { get; set; }
 
         [Column(TypeName = "nchar(1)")]
         public string? MARINERO1 { get; set; }
@@ -75,16 +139,83 @@ namespace gedefApi.Models.PlanillaRoles
 
         [Column(TypeName = "nchar(1)")]
         public string? MARINERO23 { get; set; }
+
         [Column(TypeName = "nchar(1)")]
         public string? MARINERO24 { get; set; }
+
         [Column(TypeName = "nchar(1)")]
         public string? MARINERO25 { get; set; }
+
         [Column(TypeName = "nchar(1)")]
         public string? MARINERO26 { get; set; }
+
         [Column(TypeName = "nchar(1)")]
         public string? MARINERO27 { get; set; }
+
         [Column(TypeName = "nchar(1)")]
         public string? MARINERO28 { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? MARINEROPLA1 { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? MARINEROPLA2 { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? MARINEROPLA3 { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? MARINEROPLA4 { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? MARINEROPLA5 { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? MARINEROPLA6 { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? MARINEROPLA7 { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? MARINEROPLA8 { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? MARINEROPLA9 { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? MARINEROPLA10 { get; set; }
+
+
+        [Column(TypeName = "nchar(1)")]
+        public string? MARINEROCUB1 { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? MARINEROCUB2 { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? MARINEROCUB3 { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? MARINEROCUB4 { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? MARINEROCUB5 { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? MARINEROCUB6 { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? MARINEROCUB7 { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? MARINEROCUB8 { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? MARINEROCUB9 { get; set; }
+
+        [Column(TypeName = "nchar(1)")]
+        public string? MARINEROCUB10 { get; set; }
+
 
     }
 }
--- gedefApi/Models/PlanillaRoles/RolxBarxMar.cs
+++ gedefApi/Models/PlanillaRoles/RolxBarxMar.cs
@@ -7,11 +7,32 @@ namespace gedefApi.Models.PlanillaRoles
     {
         [Key]
         public int CODROLXBARXMAR { get; set; }
+        public int? CAPITAN { get; set; }
         public int? PATRON { get; set; }
         public int? SEGUNPATRON { get; set; }
+        public int? PRIMOFCUBIERTA { get; set; }
         public int? JEFEMAQUINAS { get; set; }
         public int? SEGUNJEFEMAQUINAS { get; set; }
+        public int? PRIMOFMAQUINAS { get; set; }
+        public int? SEGUNOFMAQUINAS { get; set; }
+        public int? PRIMOFCONDUCTOR { get; set; }
+        public int? SEGUNOFCONDUCTOR { get; set; }
+        public int? PRIMPESCADOR { get; set; }
+        public int? SEGUNPESCADOR { get; set; }
+        public int? CONTRAMCUBIERTA { get; set; }
+        public int? CONTRAMPLANTA { get; set; }
         public int? COCINERO { get; set; }
+        public int? COCINERO2 { get; set; }
+        public int? ENGRASADOR1 { get; set; }
+        public int? ENGRASADOR2 { get; set; }
+        public int? BODEGUERO1 { get; set; }
+        public int? BODEGUERO2 { get; set; }
+        public int? TUNELERO1 { get; set; }
+        public int? TUNELERO2 { get; set; }
+        public int? GUINCHERO1 { get; set; }
+        public int? GUINCHERO2 { get; set; }
+        public int? LAVADOR { get; set; }
+        public int? ENFERMERO { get; set; }
         public int? MARINERO1 { get; set; }
         public int? MARINERO2 { get; set; }
         public int? MARINERO3 { get; set; }
@@ -40,7 +61,27 @@ namespace gedefApi.Models.PlanillaRoles
         public int? MARINERO26 { get; set; }
         public int? MARINERO27 { get; set; }
         public int? MARINERO28 { get; set; }
-
+        public int? MARINEROPLA1 { get; set; }
+        public int? MARINEROPLA2 { get; set; }
+        public int? MARINEROPLA3 { get; set; }
+        public int? MARINEROPLA4 { get; set; }
+        public int? MARINEROPLA5 { get; set; }
+        public int? MARINEROPLA6 { get; set; }
+        public int? MARINEROPLA7 { get; set; }
+        public int? MARINEROPLA8 { get; set; }
+        public int? MARINEROPLA9 { get; set; }
+        public int? MARINEROPLA10 { get; set; }
+        public int? MARINEROCUB1 { get; set; }
+        public int? MARINEROCUB2 { get; set; }
+        public int? MARINEROCUB3 { get; set; }
+        public int? MARINEROCUB4 { get; set; }
+        public int? MARINEROCUB5 { get; set; }
+        public int? MARINEROCUB6 { get; set; }
+        public int? MARINEROCUB7 { get; set; }
+        public int? MARINEROCUB8 { get; set; }
+        public int? MARINEROCUB9 { get; set; }
+        public int? MARINEROCUB10 { get; set; }
     }
+
 
 }
